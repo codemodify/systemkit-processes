638a5c7018343378c34650301fa92b7958aacddc
codemodify <codemodify@member.fsf.org>
2020-08-21 15:05:29 -0700

    add new StopWithTimeout

--- contracts/monitor.go
+++ contracts/monitor.go
@@ -1,11 +1,14 @@
 package contracts
 
+import "time"
+
 // Monitor - process monitor
 type Monitor interface {
 	Spawn(process ProcessTemplate) (string, error)
 	SpawnWithTag(process ProcessTemplate, tag string) error
 	Start(tag string) error
 	Stop(tag string) error
+	StopWithTimeout(tag string, attempts int, waitTimeout time.Duration) error
 	Restart(tag string) error
 	StopAll() []error
 	GetProcess(tag string) RuningProcess
